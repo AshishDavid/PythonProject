d836965978bf906490405b33febb1d8c3fe961ac
Ashish David <58704058+AshishDavid@users.noreply.github.com>
2020-11-17 19:16:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,26 +8,22 @@ def caeser():
         key = int(input('Enter the key between 0-25 \n'))
         content = file.read()
         length = len(content)
-        if sys.argv[3] == 'encryption':
-            encrypted_content = ''
-            for i in range(length):
-                if content[i].isupper():
-                    encrypted_content += chr((ord(content[i]) + key - 65) % 26 + 65)
-                if content[i].islower():
-                    encrypted_content += chr((ord(content[i]) + key - 97) % 26 + 97)
-                elif not content[i].isupper() and not content[i].islower():
-                    encrypted_content += content[i]
-            print(encrypted_content)
+        encrypted_content = ''
+        decrypted_content = ''
+        for i in range(length):
+            if content[i].isupper():
+                encrypted_content += chr((ord(content[i]) + key - 65) % 26 + 65)
+                decrypted_content += chr((ord(content[i]) - key - 65) % 26 + 65)
+            if content[i].islower():
+                encrypted_content += chr((ord(content[i]) + key - 97) % 26 + 97)
+                decrypted_content += chr((ord(content[i]) - key - 97) % 26 + 97)
+            elif not content[i].isupper() and not content[i].islower():
+                encrypted_content += content[i]
+                decrypted_content += content[i]
         if sys.argv[3] == 'decryption':
-            decrypted_content = ''
-            for i in range(length):
-                if content[i].isupper():
-                    decrypted_content += chr((ord(content[i]) - key - 65) % 26 + 65)
-                if content[i].islower():
-                    decrypted_content += chr((ord(content[i]) - key - 97) % 26 + 97)
-                elif not content[i].isupper() and not content[i].islower():
-                    decrypted_content += content[i]
             print(decrypted_content)
+        if sys.argv[3] == 'encryption':
+            print(encrypted_content)
     if sys.argv[3] == 'automatic':
         content = file.read()
         length = len(content)
@@ -52,9 +48,10 @@ def caeser():
                     else:
                         decrypted_file += content[j]
                 print(decrypted_file)
-                print('Would you like to continue with this or change the most frequent letter ?')
-                print('Press "Y" to continue and guess other letters or "N" to change the most frequent letter or '
-                      '"E"to exit')
+                print('Would you like to continue with this or change the most frequent'
+                      ' letter ?')
+                print('Press "Y" to continue and guess other letters or "N" to change the most'
+                      ' frequent letter or "E" to exit')
                 choice = input()
                 if choice == 'E':
                     file.close()
@@ -95,42 +92,39 @@ def caeser():
 def vignere():
     file = open(sys.argv[1], 'r')
     key = input('Enter the alphabetical key \n')
-    content = file.read()
-    length = len(content)
-    length_with_only_letters = 0
-    for x in range(length):
-        if content[x].isalpha():
-            length_with_only_letters += 1
-    count = length_with_only_letters - len(key)
-    c = 0
-    for x in range(count):
-        if c != length:
-            key += key[c]
-            c += 1
-        else:
-            c = 0
-            key += key[c]
-    print(key)
-    if sys.argv[3] == 'encryption':
-        encrypted_content = ''
-        key_count = 0
-        for i in range(length):
-            if content[i].isalpha():
-                encrypted_content += chr(((ord(content[i]) + ord(key[key_count])) % 26) + 65)
-                key_count += 1
+    if sys.argv[3] == 'encryption' or sys.argv[3] == 'decryption':
+        content = file.read()
+        length = len(content)
+        content = content.upper()
+        length_with_only_letters = 0
+        for x in range(length):
+            if content[x].isalpha():
+                length_with_only_letters += 1
+        count = length_with_only_letters - len(key)
+        c = 0
+        for x in range(count):
+            if c != length:
+                key += key[c]
+                c += 1
             else:
-                encrypted_content += content[i]
-        print(encrypted_content)
-    if sys.argv[3] == 'decryption':
+                c = 0
+                key += key[c]
+        key = key.upper()
+        encrypted_content = ''
         decrypted_content = ''
         key_count = 0
         for i in range(length):
-            if content[i].isalpha():
+            if content[i].isupper():
+                encrypted_content += chr(((ord(content[i]) + ord(key[key_count])) % 26) + 65)
                 decrypted_content += chr(((ord(content[i]) - ord(key[key_count]) + 26) % 26) + 65)
                 key_count += 1
             else:
+                encrypted_content += content[i]
                 decrypted_content += content[i]
-        print(decrypted_content)
+        if sys.argv[3] == 'encryption':
+            print(encrypted_content)
+        if sys.argv[3] == 'decryption':
+            print(decrypted_content)
     elif sys.argv[3] != 'encryption' and sys.argv[3] != 'decryption':
         print('No such function available')
     file.close()
@@ -139,61 +133,53 @@ def vignere():
 def vernam():
     file = open(sys.argv[1], 'r')
     content = file.read()
-    count_only_letters = 0
-    length = len(content)
-    for x in range(length):
-        if content[x].isalpha():
-            count_only_letters += 1
-    letters = str(count_only_letters)
-    key = input('Enter the keyword. It should have ' + letters+' alphabets \n')
-    if len(key) != count_only_letters:
-        print('Not a suitable keyword')
-        return
-    if sys.argv[3] == 'encryption':
-        encrypted_content = ''
-        key_count = 0
-        for i in range(length):
-            if content[i].isalpha():
-                if content[i].islower():
-                    sum_of = ord(content[i]) + ord(key[key_count]) - 194
-                    if sum_of > 25:
-                        encrypted_content += chr(sum_of - 26 + 97)
-                    else:
-                        encrypted_content += chr(sum_of + 97)
-                    key_count += 1
-                if content[i].isupper():
-                    sum_of = ord(content[i]) + ord(key[key_count]) - 130
-                    if sum_of > 25:
-                        encrypted_content += chr(sum_of - 26 + 65)
-                    else:
-                        encrypted_content += chr(sum_of + 65)
-                    key_count += 1
-            elif not content[i].isalpha():
-                encrypted_content += content[i]
-        print(encrypted_content)
-    if sys.argv[3] == 'decryption':
+    if sys.argv[3] == 'encryption' or sys.argv[3] == 'decryption':
+        count_only_letters = 0
+        length = len(content)
+        for x in range(length):
+            if content[x].isalpha():
+                count_only_letters += 1
+        letters = str(count_only_letters)
+        key = input('Enter the keyword. It should have ' + letters + 'alphabets without any space and special '
+                                                                     'characters\n')
+        if len(key) != count_only_letters:
+            print('Not a suitable keyword')
+            return
         decrypted_content = ''
         key_count = 0
-        sum_of_ = 0
+        encrypted_content = ''
         for i in range(length):
-            if content[i].isalpha():
-                if content[i].islower():
-                    if (ord(content[i]) - 97) - (ord(key[key_count]) - 97) < 0:
-                        sum_of_ = 26 + (ord(content[i]) - 97) - (ord(key[key_count]) - 97)
-                    else:
-                        sum_of_ = (ord(content[i]) - 97) - (ord(key[key_count]) - 97)
-                    decrypted_content += chr(sum_of_ + 97)
-                    key_count += 1
-                if content[i].isupper():
-                    if (ord(content[i]) - 65) - (ord(key[key_count]) - 65) < 0:
-                        sum_of_ = 26 + (ord(content[i]) - 65) - (ord(key[key_count]) - 65)
-                    else:
-                        sum_of_ = (ord(content[i]) - 97) - (ord(key[key_count]) - 97)
-                    decrypted_content += chr(sum_of_ + 65)
-                    key_count += 1
-            elif not content[i].isalpha():
+            if content[i].islower():
+                sum_of = ord(content[i]) + ord(key[key_count]) - 194
+                if sum_of > 25:
+                    encrypted_content += chr(sum_of - 26 + 97)
+                else:
+                    encrypted_content += chr(sum_of + 97)
+                if (ord(content[i]) - 97) - (ord(key[key_count]) - 97) < 0:
+                    sum_of_ = 26 + (ord(content[i]) - 97) - (ord(key[key_count]) - 97)
+                else:
+                    sum_of_ = (ord(content[i]) - 97) - (ord(key[key_count]) - 97)
+                decrypted_content += chr(sum_of_ + 97)
+                key_count += 1
+            if content[i].isupper():
+                sum_of = ord(content[i]) + ord(key[key_count]) - 130
+                if sum_of > 25:
+                    encrypted_content += chr(sum_of - 26 + 65)
+                else:
+                    encrypted_content += chr(sum_of + 65)
+                if (ord(content[i]) - 65) - (ord(key[key_count]) - 65) < 0:
+                    sum_of_ = 26 + (ord(content[i]) - 65) - (ord(key[key_count]) - 65)
+                else:
+                    sum_of_ = (ord(content[i]) - 97) - (ord(key[key_count]) - 97)
+                decrypted_content += chr(sum_of_ + 65)
+                key_count += 1
+            elif not content[i].islower() and not content[i].isupper():
+                encrypted_content += content[i]
                 decrypted_content += content[i]
-        print(decrypted_content)
+        if sys.argv[3] == 'encryption':
+            print(encrypted_content)
+        if sys.argv[3] == 'decryption':
+            print(decrypted_content)
     elif sys.argv[3] != 'encryption' and sys.argv[3] != 'decryption':
         print('No such function available')
     file.close()
@@ -209,10 +195,7 @@ else:
             vignere()
         if sys.argv[2] == 'vernam':
             vernam()
+        else:
+            print('No such cipher available')
     else:
         print('No such file exists')
-
-# argv[0] - main.py
-# argv[1] - .txt file path
-# argv[2] - type of cipher
-# argv[3] - encryption/decryption
